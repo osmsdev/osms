062d2bf0444d1a1b9efee11d85030c0fab082a8e
Nimesh Morkar <nmorkar@gmail.com>
2014-08-07 19:18:06 +0530

    updated

---
+++ osms-api/src/main/java/com/jprnd/osms/api/controller/SystemResource.java
@@ -0,0 +1,51 @@
+package com.jprnd.osms.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.jprnd.osms.services.LoginService;
+
+@Controller
+@RequestMapping(value = SystemResource.URL_PREFIX_SYSTEM)
+public class SystemResource extends AbstractBaseResource {
+    static final String URL_PREFIX_SYSTEM = "/api/system";
+
+    private static final String API_VERSION = "2.0";
+    	
+    private static final String AUTHENTICATE = "/authenticate";
+    private static final String API_INFO = "/apiinfo";
+
+    public static final String USER_PARAM = "user";
+    private static final String PASSWORD_PARAM = "password";
+
+    // package scope for accessibility from unit-tests
+    int securityTokenExpirationSec;
+
+   
+    @Autowired
+    private LoginService loginService;
+
+   
+
+    @RequestMapping(value = API_INFO, method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ResponseEntity<String> getApiInfo() {
+    	System.out.println("CAlling ApiInfo");
+    	loginService.dummy();
+        return generateOk("{ \"api_version\":\"" + API_VERSION + "\" }");
+    }
+
+    /**
+     * Some requests do not require authentication.
+     */
+    public static boolean isAuthenticationRequired(final String requestURI) {
+        return !(requestURI.endsWith(API_INFO) || requestURI.endsWith(AUTHENTICATE));
+    	//return !(requestURI.endsWith(API_INFO) || requestURI.endsWith(AUTHENTICATE) || requestURI.endsWith(REPORT));
+    }
+
+}
