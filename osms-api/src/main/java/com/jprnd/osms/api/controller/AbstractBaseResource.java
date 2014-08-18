5c12739f800834b714965ba65b538c0c77435ea0
Nimesh Morkar <nmorkar@gmail.com>
2014-08-18 15:22:34 +0530

    updated

--- osms-api/src/main/java/com/jprnd/osms/api/controller/AbstractBaseResource.java
+++ osms-api/src/main/java/com/jprnd/osms/api/controller/AbstractBaseResource.java
@@ -15,9 +15,9 @@ public abstract class AbstractBaseResource {
     public static final String PARAM_SECURITY_TOKEN = "security_token";
     protected static final String PATH_VAR_ID = "id";
 
-    protected static final ResponseEntity<String> NOT_FOUND = new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    protected static final ResponseEntity<String> NO_CONTENT = new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    protected static final ResponseEntity<String> INTERNAL_ERROR = new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    protected static final ResponseEntity<String> NOT_FOUND = new ResponseEntity(HttpStatus.NOT_FOUND);
+    protected static final ResponseEntity<String> NO_CONTENT = new ResponseEntity(HttpStatus.NO_CONTENT);
+    protected static final ResponseEntity<String> INTERNAL_ERROR = new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
 
     public static final String USER_REQ_ATTR = "__USER";
 
@@ -29,7 +29,7 @@ public abstract class AbstractBaseResource {
     }
 
     protected ResponseEntity<String> generateOk(final String responseBody) {
-        return new ResponseEntity<>(responseBody, HttpStatus.OK);
+        return new ResponseEntity(responseBody, HttpStatus.OK);
     }
 
     
